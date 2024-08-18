450ab3e76dc398a113b82a126d1328259c0cf345
akxotjr <133089882+akxotjr@users.noreply.github.com>
2024-08-18 16:20:11 +0900

    123

--- VampireSurvivals/StatUpgradeScene.cpp
+++ VampireSurvivals/StatUpgradeScene.cpp
@@ -35,6 +35,8 @@ StatUpgradeScene::StatUpgradeScene()
 	}
 
 	ResourceManager::GetInstance()->LoadTexture(L"Stat-LevelUP-Gauge", L"UI\\Stat-LevelUP-Gauge.bmp");
+
+	ResourceManager::GetInstance()->LoadFont(L"m3x6", L"Font\\m3x6.ttf");
 }
 
 StatUpgradeScene::~StatUpgradeScene()
@@ -54,6 +56,28 @@ void StatUpgradeScene::Update()
 void StatUpgradeScene::Render(HDC hdc)
 {
 	Super::Render(hdc);
+
+	{
+		HFONT hFont = ResourceManager::GetInstance()->GetFont(L"m3x6");
+
+		if (hFont) {
+			HFONT hOldFont = (HFONT)SelectObject(hdc, hFont);
+
+			// 글자 색상 설정
+			//SetTextColor(hdc, RGB(0, 0, 255)); // 파란색
+
+			// 배경 투명도 설정
+			//SetBkMode(hdc, TRANSPARENT);
+
+			// 텍스트 출력
+			TextOut(hdc, 20, 20, L"asdfasdfasdf", 12);
+
+			SelectObject(hdc, hOldFont);
+		}
+		else {
+			MessageBox(NULL, L"폰트가 적용되지 않았습니다.", L"Error", MB_OK);
+		}
+	}
 }
 
 void StatUpgradeScene::AtkUpgradeButton()
